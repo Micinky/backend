50a8d6ca2cf9c6258da0e4f8fd59409d10cc2b7d
radimkohout <radimm.kohout@gmail.com>
2022-10-13 16:23:33 +0200

    Update api.php

--- api.php
+++ api.php
@@ -6,8 +6,10 @@ if ($type == 0) {
     $url = "";
     $res = GetFromApi(base64_decode($ke));
     for ($i = 0; $i < count($res); $i++) {
-        $lat = $res[$i]['geometry']["y"]||explode($res[$i]['geometry']["coordinates"],',')[0];
-        $lon = $res[$i]['geometry']["x"]||explode($res[$i]['geometry']["coordinates"],',')[1];
+        $lat = $res[$i]['geometry']["y"];
+        if($lat="") $lat = explode($res[$i]['geometry']["coordinates"],',')[0];
+        $lon = $res[$i]['geometry']["x"];
+        if($lon="") $lon = explode($res[$i]['geometry']["coordinates"],',')[1];
         if (isset($res[$i]['attributes'])) {
             $name = $res[$i]['attributes']["Název"];
             unset($res[$i]['attributes']["Název"]);
